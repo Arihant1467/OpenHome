31195f16f2eb79021042383bad80a051e6024523
deepikay912 <deepika640yannamani@gmail.com>
2019-12-04 13:03:52 -0800

    time advancement backend

---
+++ OpenHome-server/src/main/java/com/cmpe275/OpenHome/service/TimeAdvancementServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cmpe275.OpenHome.service;
+
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+
+@Repository
+public class TimeAdvancementServiceImpl {
+
+
+    private  LocalDateTime currentTime = LocalDateTime.now();
+
+
+    public LocalDateTime getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(LocalDateTime currentTime) {
+        this.currentTime = currentTime;
+    }
+}
