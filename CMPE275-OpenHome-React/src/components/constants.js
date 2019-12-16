fc541b240654efdd3f45a165332dfd3e9ece6f4f
Arihant1467 <arihant95@gmail.com>
2019-12-16 14:40:47 -0800

    -

--- CMPE275-OpenHome-React/src/components/constants.js
+++ CMPE275-OpenHome-React/src/components/constants.js
@@ -1,5 +1,5 @@
 
 export const BASE_URL = process.env.JAVA_BACKEND==null ? "http://localhost:8080/OpenHome_war/api": process.env.JAVA_BACKEND;
-//export const FRONTEND_URL = process.env.HEROKU_URL==null ? "http://localhost:3000/": process.env.HEROKU_URL;
+export const FRONTEND_URL = process.env.HEROKU_URL==null ? "http://localhost:3000/": process.env.HEROKU_URL;
 export const IMG_RETRIEVE_BASE_URL = process.env.IMAGE_CDN==null ?  "http://localhost:3500": process.env.IMAGE_CDN;
 export const API_KEY = process.env.API_KEY;
--- OpenHome-frontend/src/components/constants.js
+++ OpenHome-frontend/src/components/constants.js
@@ -1,8 +1,5 @@
 
-export const BASE_URL = "http://34.221.96.59/:8080/OpenHome_war/api"
-// export const FRONTEND_URL = "http://localhost:3000/"
-export const FRONTEND_URL = "https://cmpe275-savd-openhome.herokuapp.com/"
-
-export const IMG_RETRIEVE_BASE_URL = "http://104.154.216.198:3500";// "http://localhost:3500";
-
-
+export const BASE_URL = process.env.JAVA_BACKEND==null ? "http://localhost:8080/OpenHome_war/api": process.env.JAVA_BACKEND;
+export const FRONTEND_URL = process.env.HEROKU_URL==null ? "http://localhost:3000/": process.env.HEROKU_URL;
+export const IMG_RETRIEVE_BASE_URL = process.env.IMAGE_CDN==null ?  "http://localhost:3500": process.env.IMAGE_CDN;
+export const API_KEY = process.env.API_KEY;
