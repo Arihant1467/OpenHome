3137021a8beb1f0aac10076a0a64b312c89c6292
Vaishnavi Ramesh <vramesh@pulsesecure.net>
2019-12-16 01:45:10 -0800

    Property Overview Edit

--- OpenHome-frontend/src/components/PropertyOverview/PropertyOverview.js
+++ OpenHome-frontend/src/components/PropertyOverview/PropertyOverview.js
@@ -37,7 +37,7 @@ class PropertyOverview extends Component {
     componentDidMount() {
 
         const { propertyId } = this.state;
-        `${BASE_URL}/posting/${propertyId}`
+       // `${BASE_URL}/posting/${propertyId}`
         axios.get(`${BASE_URL}/posting/${propertyId}`).then((response) => {
             const data = response.data;
             this.setState({
